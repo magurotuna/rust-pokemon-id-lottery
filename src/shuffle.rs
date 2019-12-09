da40c126256ade7a90672324d44c423b5bb58b44
Yusuke Tanaka <wing0920@gmail.com>
2019-12-09 23:58:51 +0900

    :green_heart: add shuffle test

--- src/shuffle.rs
+++ src/shuffle.rs
@@ -6,3 +6,13 @@ pub fn get_shuffled_ids(rng: &mut StdRng) -> Vec<u32> {
     pokemon_ids_base.shuffle(rng);
     pokemon_ids_base
 }
+
+#[test]
+fn test_get_shuffled_ids() {
+    let mut rng = StdRng::from_seed([0u8; 32]);
+
+    assert_eq!(
+        get_shuffled_ids(&mut rng).get(0..3),
+        Some(&[159668, 343287, 402966][..])
+    );
+}
