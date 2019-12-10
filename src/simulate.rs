06c9b3298e6f6f7e0b9df391eefd2b2224a79e17
Yusuke Tanaka <wing0920@gmail.com>
2019-12-10 22:22:38 +0900

    :green_heart: add test_simulate_oneday

--- src/simulate.rs
+++ src/simulate.rs
@@ -32,7 +32,6 @@ pub fn exec_simulation(
         .collect()
 }
 
-// TODO: test this function
 fn simulate_oneday(ids: &[u32], number_this_day: u32) -> Award {
     let mut award = Award::Losing;
     let win_number = format!("{:05}", number_this_day);
@@ -52,4 +51,48 @@ fn simulate_oneday(ids: &[u32], number_this_day: u32) -> Award {
         }
     }
     award
+}
+
+#[test]
+fn test_simulate_oneday() {
+    let ids = vec![0u32, 111111, 222222, 345678];
+    // 5 digits match
+    assert_eq!(simulate_oneday(&ids, 0), Award::First);
+    assert_eq!(simulate_oneday(&ids, 11111), Award::First);
+    assert_eq!(simulate_oneday(&ids, 22222), Award::First);
+    assert_eq!(simulate_oneday(&ids, 34567), Award::First);
+    assert_eq!(simulate_oneday(&ids, 45678), Award::First);
+
+    // last 4 digits match
+    assert_eq!(simulate_oneday(&ids, 1111), Award::Second);
+    assert_eq!(simulate_oneday(&ids, 2222), Award::Second);
+    assert_eq!(simulate_oneday(&ids, 91111), Award::Second);
+    assert_eq!(simulate_oneday(&ids, 92222), Award::Second);
+    assert_eq!(simulate_oneday(&ids, 95678), Award::Second);
+
+    // last 3 digits match
+    assert_eq!(simulate_oneday(&ids, 111), Award::Third);
+    assert_eq!(simulate_oneday(&ids, 222), Award::Third);
+    assert_eq!(simulate_oneday(&ids, 99111), Award::Third);
+    assert_eq!(simulate_oneday(&ids, 99222), Award::Third);
+    assert_eq!(simulate_oneday(&ids, 99678), Award::Third);
+
+    // last 2 digits match
+    assert_eq!(simulate_oneday(&ids, 11), Award::Fourth);
+    assert_eq!(simulate_oneday(&ids, 22), Award::Fourth);
+    assert_eq!(simulate_oneday(&ids, 99911), Award::Fourth);
+    assert_eq!(simulate_oneday(&ids, 99922), Award::Fourth);
+    assert_eq!(simulate_oneday(&ids, 99978), Award::Fourth);
+
+    // last 1 digit matches
+    assert_eq!(simulate_oneday(&ids, 1), Award::Fifth);
+    assert_eq!(simulate_oneday(&ids, 2), Award::Fifth);
+    assert_eq!(simulate_oneday(&ids, 99991), Award::Fifth);
+    assert_eq!(simulate_oneday(&ids, 99992), Award::Fifth);
+    assert_eq!(simulate_oneday(&ids, 99998), Award::Fifth);
+
+    // no digit matches
+    assert_eq!(simulate_oneday(&ids, 11119), Award::Losing);
+    assert_eq!(simulate_oneday(&ids, 22229), Award::Losing);
+    assert_eq!(simulate_oneday(&ids, 99999), Award::Losing);
 }
